f5dff2b487ebdc98d0a10e0e0abe102113167b5e
MiguelAngelCabanillas <62569219+MiguelAngelCabanillas@users.noreply.github.com>
2020-05-13 19:26:48 +0200

    Add files via upload

---
+++ ConexionConBasedeDatos.java
@@ -0,0 +1,16 @@
+package BD;
+
+public class ConexionConBasedeDatos {
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "database-iis.cobadwnzalab.eu-central-1.rds.amazonaws.com";
+	static final String DB_SCHEMA = "deadwinterdb";
+
+	//  Database credentials
+	static final String USER = "kodiak";
+	static final String PASS = "isaacvega";
+
+	
+	//AQUI VAN LAS FUNCIONES DE INSERTAR, BORRAR, O CONSULTAR
+	
+	
+}
