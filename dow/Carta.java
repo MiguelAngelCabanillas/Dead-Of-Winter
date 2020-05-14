025f0cad05e25116863b1211fb49549c4847a464
manuel-agm <62695838+manuel-agm@users.noreply.github.com>
2020-05-14 19:14:25 +0200

    Update Carta.java

--- dow/Carta.java
+++ dow/Carta.java
@@ -1,1 +1,30 @@
-TIENE PELOS
+//PARA NADA VERSIÓN DEFINITIVA
+package dow;
+
+public class Carta {
+
+	private int ID;
+	private String Nombre;
+	//hay que saber como vamos a implementas las imagenes de las cartas
+	//private imagen;
+	private String descripcion;
+	
+	public Carta(int id, String nombre, String desc) {
+		ID = id;
+		Nombre = nombre;
+		descripcion = desc;
+	}
+	
+	public int getID() {
+		return this.ID;
+	}
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+}
