041dfe41b644428fe1387c7148f161eca4fe14fd
mcroldan <62695677+mcroldan@users.noreply.github.com>
2020-05-14 19:09:35 +0200

    Añadido tratamiento de desconexión
    Avast da problemas. Desactivar el antivirus o crear una excepción en el firewall

--- Server/Reader.java
+++ Server/Reader.java
@@ -1,25 +1,30 @@
 package Server;
 
+import bd.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.List;
 
 public class Reader implements Runnable {
 private Socket socket;
 private List<PrintWriter> ls;
+private List<Socket> sl;
 	
-	public Reader (Socket s, List<PrintWriter> ls) {
+	public Reader (Socket s, List<PrintWriter> ls, List<Socket> sl) {
 		socket = s;
 		this.ls = ls;
+		this.sl = sl;
 	}
 	
 	public void run() {
 		try {
 			InputStreamReader reader = new InputStreamReader(socket.getInputStream());
 			BufferedReader buffer = new BufferedReader(reader);
+			
 			
 			while (true) {
 			String mensaje = buffer.readLine();
@@ -33,6 +38,28 @@ private List<PrintWriter> ls;
 				continue;
 			}
 		 }
+		} catch (SocketException e1) {
+		 int index = sl.indexOf(socket);
+		 sl.remove(index);
+		 ls.remove(index);
+		 System.out.println("Desconectado:" + socket.getInetAddress());
+		 
+		 for(Socket s : sl) {
+				System.out.println(s.getInetAddress());
+			}
+			int i = 0;
+			for(PrintWriter pw1 : ls) {
+				i++;
+			} System.out.println(i);
+		 
+		 try {
+			socket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
