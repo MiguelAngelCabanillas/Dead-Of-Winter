6ec86010d69fe40613bef9fbc5222e9e3b73fef0
franmj <franciscomj00@uma.es>
2020-06-12 02:48:42 +0200

    durmiendo la hebra de la sala

--- src/Gui/FrameSala.java
+++ src/Gui/FrameSala.java
@@ -115,7 +115,7 @@ public class FrameSala extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
 					usuario.hacerPeticionAlServidor(usuario.getNombre() + "|1|host");
-					Thread.sleep(500);
+					Thread.sleep(100); //espera a recibir la confirmación del servidor
 				} catch (IOException e) {
 					e.printStackTrace();
 				} catch (InterruptedException e) {
