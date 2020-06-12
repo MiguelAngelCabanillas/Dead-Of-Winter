c5d2ea90cc7901b48c1c09277de8085f9cad0036
StNieto <nietojimenezmanuel@gmail.com>
2020-06-12 12:45:03 +0200

    clases de crisis

---
+++ src/Partida/InicCrisis.java
@@ -0,0 +1,28 @@
+package Partida;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class InicCrisis {
+
+	private List<Crisis> crisis = new ArrayList<>();
+	private Random r = new Random();
+	
+	public InicCrisis(int numJug) {
+		inicCrisis(numJug);
+	}
+	
+	private void inicCrisis(int numJug) {
+		Crisis aux;
+		for(int i = 0; i < 8; i ++) {
+			aux = new Crisis(i, numJug);
+			crisis.add(aux);
+		}
+	}
+	
+	public Crisis getCrisis() {
+		return crisis.remove(r.nextInt(crisis.size()));
+	}
+	
+}
