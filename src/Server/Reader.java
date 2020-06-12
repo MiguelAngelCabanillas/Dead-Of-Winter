a0f96f83e62d6c859acd88498f40915263d5e739
franmj <franciscomj00@uma.es>
2020-06-12 21:05:42 +0200

    finturno

--- src/Server/Reader.java
+++ src/Server/Reader.java
@@ -25,6 +25,7 @@ private List<Sala> salas;
 
 private InputStreamReader reader;
 private BufferedReader buffer;
+private static int cuchillo = 0;
 
 
 	
@@ -215,7 +216,7 @@ private BufferedReader buffer;
 					  if(split[3].equalsIgnoreCase("sala")) {
 						 salirDeSala(user, user.getSala());
 					  } else if(split[3].equalsIgnoreCase("tablero")) { // Inicializar partida
-						 
+						  
 						  String mensInit = "initSup";
 						  String mensIds = "ids";
 					   if(user.getSala().getHost().getNombre().equals(user.getNombre())) {
@@ -278,8 +279,8 @@ private BufferedReader buffer;
 							  //ENVIA LOS DADOS A CADA JUGADOR//
 
 							  user.getSala().getPartida().inicDados();
-							  user.getSala().getUsuarios().get(i).hacerPeticionAlServidor("newRound|" + user.getSala().getPartida().getRondasRestantes() + "|303|" + user.getSala().getPartida().getDados(i));
-
+							  user.getSala().getUsuarios().get(i).hacerPeticionAlServidor("newRound|" + user.getSala().getPartida().getRondasRestantes() + "|" + user.getSala().getPartida().getCrisisActual() + "|" + user.getSala().getPartida().getDados(i));
+							  user.getSala().getUsuarios().get(i).hacerPeticionAlServidor("moral|" + user.getSala().getPartida().getMoral());
 							  
 							  for(Usuario usu : user.getSala().getUsuarios()){
 								  System.out.println(user.getJugador().getMazoSuperviviente().size() + " " + user.getNombre());
@@ -288,17 +289,36 @@ private BufferedReader buffer;
 							  
 							  
 							  System.out.println("initCartas|" + user.getSala().getPartida().getIdCartas(i));
-							  System.out.println("newRound|" + user.getSala().getPartida().getRondasRestantes() + "|303|" + user.getSala().getPartida().getDados(i));
+							  System.out.println("newRound|" + user.getSala().getPartida().getRondasRestantes() + "|" + user.getSala().getPartida().getCrisisActual() + "|" + user.getSala().getPartida().getDados(i));
 						  }
+						  
+						  user.getSala().getUsuarios().get(cuchillo).hacerPeticionAlServidor("tuturno");
+						  user.enviarALaSala("chat|Turno de " + user.getSala().getUsuarios().get(cuchillo).getNombre());
+						  
 					   }
-						  
-						  
-						  
-						 }
+					
+					}
 					 break;
+				case "finturno": //finturno|idJugAnterior
+					int idSig = (Integer.parseInt(split[3]) + 1)%user.getSala().getUsuarios().size();
+					if(idSig == cuchillo) {
+						cuchillo = (cuchillo + 1)%user.getSala().getUsuarios().size();
+						user.getSala().getPartida().pasaRonda();
+						for(int i = 0; i < user.getSala().getUsuarios().size(); i++) {
+							user.getSala().getUsuarios().get(i).hacerPeticionAlServidor("newRound|" + user.getSala().getPartida().getRondasRestantes() + "|" + user.getSala().getPartida().getCrisisActual() + "|" + user.getSala().getPartida().getDados(i));
+							user.getSala().getUsuarios().get(i).hacerPeticionAlServidor("moral" + user.getSala().getPartida().getMoral());
+						}
+						user.getSala().getUsuarios().get(cuchillo).hacerPeticionAlServidor("tuturno");
+						user.enviarALaSala("chat|Turno de " + user.getSala().getUsuarios().get(cuchillo).getNombre());
+					} else {
+						user.getSala().getPartida().pasaTurno(idSig);
+						user.getSala().getUsuarios().get(idSig).hacerPeticionAlServidor("tuturno");
+						user.enviarALaSala("chat|Turno de " + user.getSala().getUsuarios().get(idSig).getNombre());
+					}
+					break;
 				case "newRound": // Me hace falta la crisis
 					for(Usuario usario : user.getSala().getUsuarios()) {
-					usario.hacerPeticionAlServidor("newRound|" + user.getSala().getPartida().getRonda() + "|1" /*user.getSala().getPartida().getCrisis()*/ + "|" + user.getSala().getPartida().getDados(usario.getJugador().getId()));
+						usario.hacerPeticionAlServidor("newRound|" + user.getSala().getPartida().getRonda() + "|"  + user.getSala().getPartida().getCrisisActual() + "|" + user.getSala().getPartida().getDados(usario.getJugador().getId()));
 					}
 					break;
 				case "moral":
